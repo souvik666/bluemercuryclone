6b69c3f53ae4a7fcdbcabf69b41271b5d3eb32aa
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-28 02:22:30 +0530

    Minor Change

--- src/server.js
+++ src/server.js
@@ -15,8 +15,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app
-
 app.listen("1234", async () => {
     await connect();
     console.log("Listening on port 1234....");
