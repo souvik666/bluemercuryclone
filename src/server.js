65425d9dc31efd014a50fb52da9254fecd31f692
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-28 02:12:23 +0530

    Basic Structure

--- src/server.js
+++ src/server.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(express.json());
 
+// app.use(express.urlencoded());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app
+
 app.listen("1234", async () => {
     await connect();
     console.log("Listening on port 1234....");
