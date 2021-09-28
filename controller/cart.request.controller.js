3580ba9540ca0491077e207af08f41ab4bb8cf2e
souvik666 <peacedynasty0@gmail.com>
2021-09-28 21:56:27 +0530

    every major bugs fixed and add to cart api added

---
+++ controller/cart.request.controller.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const products = require("../models/product.model");
+const myClients = require("../models/user.model");
+/* add to cart request by client */
+router.get("/:userID/:productID", async (req, res) => {
+  const whoami = req.params.userID; //user ID
+  const product_id = req.params.productID;
+  //see if already exsited product
+  const findPro = await myClients.find({
+    $and: [{ _id: whoami }, { cart: product_id }],
+  });
+
+  /*  const myUsrs = await myClients.findByIdAndUpdate(whoami, {
+    $push: { cart: product_id },
+  }); */
+  //const tmp = await products.find();
+  res.send(findPro);
+  //res.send(myUsrs);
+});
+
+module.exports = router;
--- controller/product.controller.js
+++ controller/product.controller.js
@@ -4,16 +4,17 @@ const router = express.Router();
 
 const Products = require("../models/product.model");
 
-router.post("", async (req, res) => {
-    const items = await Products.create(req.body);
-    return res.send({items});
-})
+router.post("/", async (req, res) => {
+  const items = await Products.create(req.body);
+  return res.send({ items });
+});
 
-router.get("", async (req, res) => {
-    const items = await Products.find().lean().exec();
-    return res.render("new.ejs", {
-        items,
-    })
-})
+router.get("/:id", async (req, res) => {
+  const items = await Products.find().sort({ price: -1 });
+  res.render("new.ejs", {
+    items,
+  });
+  // res.send(items);
+});
 
 module.exports = router;
