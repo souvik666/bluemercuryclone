cf0bf294d57e263c75be5592316547db3248ded1
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-28 22:10:00 +0530

    Minor Change

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,19 +1,39 @@
+const { application } = require("express");
 const express = require("express");
 
 const router = express.Router();
 
 const Products = require("../models/product.model");
+// const user = require("../models/user.model");
+
+let items;
 
 router.post("", async (req, res) => {
-    const items = await Products.create(req.body);
+    items = await Products.create(req.body);
     return res.send({items});
 })
 
 router.get("", async (req, res) => {
-    const items = await Products.find().lean().exec();
+    items = await Products.find().lean().exec();
+    // const userRes = req.params.hook;
+    // if (user.res === "ltoh") {
+        // items = await Products.find().sort({"price": -1}).lean().exec();
+    // } else {
+        // items = await Products.find().lean().exec();
+    // }
+    // items = 
+    
     return res.render("new.ejs", {
-        items,
+        items
     })
 })
+
+// router.get("/lToH", async (req, res) => {
+//     const ltoh = await Products.find().sort({"price": 1}).lean().exec();
+//     items = ltoh;
+//     return res.render("new.ejs", {
+//         items
+//     })
+// })
 
 module.exports = router;
