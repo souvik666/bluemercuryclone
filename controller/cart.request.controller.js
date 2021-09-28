bf79b4310a2692a62e99ee7efd0615bc18521002
souvik666 <peacedynasty0@gmail.com>
2021-09-28 23:45:15 +0530

    full feturdcart

--- controller/cart.request.controller.js
+++ controller/cart.request.controller.js
@@ -2,21 +2,46 @@ const express = require("express");
 const router = express.Router();
 const products = require("../models/product.model");
 const myClients = require("../models/user.model");
+
+/* check if the product already exsit */
+const counter = async (arr) => {
+  let map = {};
+  let tmp;
+  for (let i in arr) {
+    tmp = arr[i];
+    map[tmp] = map[tmp] || 0;
+    map[tmp]++;
+  }
+  return map;
+};
 /* add to cart request by client */
 router.get("/:userID/:productID", async (req, res) => {
   const whoami = req.params.userID; //user ID
   const product_id = req.params.productID;
   //see if already exsited product
-  const findPro = await myClients.find({
+  /*   {
+    $push: { cart: product_id },
+  } */
+  const findPro = await myClients.findOne({
     $and: [{ _id: whoami }, { cart: product_id }],
   });
-//few logic left/*  */
-  /*  const myUsrs = await myClients.findByIdAndUpdate(whoami, {
-    $push: { cart: product_id },
-  }); */
-  //const tmp = await products.find();
-  res.send(findPro);
-  //res.send(myUsrs);
+  let { cart } = findPro;
+  cart.push(product_id);
+  let newCart = await counter(cart);
+  console.log(newCart);
+  if (findPro) {
+    await myClients.findByIdAndUpdate(
+      whoami,
+      [{ $set: { newcart: [newCart] } }],
+      {
+        new: true,
+      }
+    );
+    await myClients.findByIdAndUpdate(whoami, [{ $set: { cart: [] } }], {
+      new: true,
+    });
+  }
+  res.send(await myClients.find());
 });
 
 module.exports = router;
