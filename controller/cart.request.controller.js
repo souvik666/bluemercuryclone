3525655dc0113c8e65a45dba6f22de9ab17c37c0
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-10-01 02:27:44 +0530

    Changed code according to need. Completed!

--- controller/cart.request.controller.js
+++ controller/cart.request.controller.js
@@ -2,11 +2,31 @@ const express = require("express");
 const router = express.Router();
 const products = require("../models/product.model");
 const myClients = require("../models/user.model");
+const { LocalStorage } = require("node-localstorage");
+var localStorage = new LocalStorage("./scratch");
 
 router.get("", async (req, res) => {
-  return res.render("cart.ejs");
+  let userId = localStorage.getItem("userid");
+  const user = await myClients.findById(userId); // user
+  const prods = user.newCart;
+  let prodsArr = []; // Products in cart 
+  let quantityArr = []; // Quantity of Products in cart
+  let cartProducts = Object.keys(prods[0]);
+  let cartQuantity = Object.values(prods[0]);
+  // console.log(cartProducts)
+  if (cartProducts.length > 0) {
+    for (let i = 0; i < cartProducts.length; i++) {
+        const searchProd = await products.findById(cartProducts[i]);
+        prodsArr.push(searchProd);
+        quantityArr.push(cartQuantity[i]);
+    }
+  }
+  return res.render("cart.ejs", {
+    user,
+    prodsArr,
+    quantityArr
+  });
 })
-
 
 /* check if the product already exsit */
 const counter = async (arr) => {
@@ -42,21 +62,22 @@ router.get("/total/:id", async (req, res) => {
 /* add to cart request by client */
 router.get("/:userID/:productID", async (req, res) => {
   const whoami = req.params.userID; //user ID
-  const product_id = req.params.productID;
-
+  const product_id = req.params.productID;  
   const findPro = await myClients.findByIdAndUpdate(whoami, {
     $push: { cart: product_id },
   });
   let { cart, newCart } = findPro;
-  for (let i in newCart) {
-    cart.push(Object.keys(newCart[i]).toString());
-  }
+  // console.log(newCart)
+  // for (let i in newCart) {
+  //   cart.push(Object.keys(newCart[i]).toString());
+  // }
   /*  see if already exsited product*/
   let countedData = await counter(cart);
   await myClients.findByIdAndUpdate(whoami, {
     newCart: countedData,
   });
-  res.send(findPro);
+  // res.send(findPro);
+  return res.redirect("/product"); 
 });
 
 /* showProducts on cart */
