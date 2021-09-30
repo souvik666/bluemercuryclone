15cbff7f3d87d04a007d35cad00556b38927a961
souvik666 <peacedynasty0@gmail.com>
2021-09-30 09:40:03 +0530

    few bug fix

--- controller/forgot.controller.js
+++ controller/forgot.controller.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const user = require("../models/user.model");
 
-router.patch("/:id", async (req, res) => {
+router.patch("/:email", async (req, res) => {
   let body = req.body.password;
   const myres = await user.findOneAndUpdate(
-    req.params.id,
+    req.params.email,
     { password: body },
     { new: true }
   );
