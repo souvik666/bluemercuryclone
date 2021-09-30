2fe5b1b77cabc3c6df89d274c3a0802c33e0c1ee
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-10-01 02:25:27 +0530

    Passing User Details via Route

--- controller/productdetails.controller.js
+++ controller/productdetails.controller.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const products = require("../models/product.model");
+const { LocalStorage } = require("node-localstorage");
+var localStorage = new LocalStorage("./scratch");
 /* get my product details*/
 /* req url format ---> localhost:2345/productdetails/6152e6ab9491bf5becda5e75 */
 
@@ -13,8 +15,10 @@ let findPro = products.find({name: "Orange Flower Water"})
 router.get("/:id", async (req, res) => {
   const product_id = req.params.id;
   findPro = await products.findOne({_id: req.params.id}).lean().exec();
+  let loggedUser = localStorage.getItem("userid");
   res.render("product_details.ejs", {
-      findPro
+      findPro,
+      loggedUser
   });
 });
 
