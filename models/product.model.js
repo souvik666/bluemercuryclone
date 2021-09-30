b1bb4508d0f444642a12da54313769742fa64d47
sHikha06-max <shikha_fw10_212@masai.school>
2021-09-30 13:36:21 +0530

    upload

--- models/product.model.js
+++ models/product.model.js
@@ -7,7 +7,8 @@ const productSchema = mongoose.Schema({
     price: {type: Number, require: true},
     category: {type: String, require: true},
     description: {type: String, require: true},
-    details: {type: String, require: true}
+    details: { type: String, require: true },
+     status: {type: String, require: true}
 }, {
     versionKey: false,
     timestamps: true
