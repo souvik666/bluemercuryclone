4ede721cfbd457b0da8ec71343d27bb316245cec
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-30 16:01:51 +0530

    Login Status Feature

--- controller/homepage.controller.js
+++ controller/homepage.controller.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const products = require("../models/product.model");
+const users = require("../models/user.model");
+const { LocalStorage } = require("node-localstorage");
+var localStorage = new LocalStorage("./scratch");
+
+var loginStatus;
+
 router.get("/", async (req, res) => {
   let product = await products.find();
+  let isLogged = localStorage.getItem("isloggedIn");
+  if (isLogged === "true") {
+    loginStatus = "LOGOUT";
+  } else {
+    loginStatus = "SIGN IN/UP";
+  }
+  
   res.render("index", {
     product,
+    loginStatus
   });
 });
 
