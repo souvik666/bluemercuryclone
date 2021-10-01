6ee6dda77bb6b7b650c5b01203d5506cb80dd866
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-10-01 16:37:28 +0530

    Added New Routes

--- src/server.js
+++ src/server.js
@@ -3,13 +3,6 @@ const connect = require("./configs/db");
 const https = require("https");
 const qs = require("querystring");
 
-// flash msg ----------
-// const session = require('express-session');
-// const path = require('path');
-// const {flash} = require('express-flash-message');
-// const flash = require('connect-flash');
-// flash msg ----------
-
 const app = express();
 //controllers
 const userController = require("../controller/usersignup.controller.js");
@@ -17,33 +10,33 @@ const loginController = require("../controller/userlogin.controller.js");
 const productController = require("../controller/product.controller.js");
 const addtocartController = require("../controller/cart.request.controller");
 const homePageController = require("../controller/homepage.controller.js");
-// const forgotController = require("../controller/forgot.controller");
 const paymentController = require("../controller/payment2.controller");
-const shippingController = require("../controller/payment.controller");
+const shippingController = require("../controller/shipping.controller");
 const productDetailsController = require("../controller/productdetails.controller");
 const passwordController = require("../controller/forgot.controller");
+const paytmController = require("../controller/paytm.controller");
+const cardDetailsController = require("../controller/carddetails.controller");
+const paytmPaymentController = require("../controller/payment.controller");
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 
-// flash message express session----------
-// app.use(session({secret: 'successfull', resave: false, saveUninitialized: true}));
-// apply express-flash-message middleware
-// app.use(flash());
-// ---------------------------------------
 
 app.use("/signup", userController);
 app.use("/login", loginController);
 app.use("/product", productController);
 app.use("/addtocart", addtocartController);
 app.use("/", homePageController);
-// app.use("/forgot", forgotController);
 app.use("/payment", paymentController);
 app.use("/shipping", shippingController);
 app.use("/productdetails", productDetailsController);
-app.use("/forgetpass", passwordController); //patch req
+app.use("/forgetpass", passwordController);
+app.use("/paytm", paytmController);
+app.use("/carddetails", cardDetailsController);
+app.use("/paytmpayment", paytmPaymentController);
+
 app.listen(2345, async (req, res) => {
   await connect();
   console.log("Listening on port 2345....");
