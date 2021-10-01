e43847a6b9227947b80eb143a1a68be074262291
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-10-01 12:01:40 +0530

    Roter to get Paytm Page

--- controller/payment2.controller.js
+++ controller/payment2.controller.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get("", async (req, res) => {
     return res.render("payment_2.ejs");
-  })
+})
+
+router.get("/paytm", async (req, res) => {
+  return res.render("testpayment.ejs");
+})
 
 module.exports = router;
