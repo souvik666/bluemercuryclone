082919d54f1873ac463f431dfd23da8057815cf0
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-10-01 16:34:09 +0530

    Routes for Shipping and Promo

--- controller/payment2.controller.js
+++ controller/payment2.controller.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("", async (req, res) => {
-    return res.render("payment_2.ejs");
+
+router.post("/:amount", async (req, res) => {
+    const email = req.body.email;
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const address = req.body.address;
+    const city = req.body.city;
+    const state = req.body.state;
+    const pincode = req.body.pincode;
+    const amount = req.params.amount;
+    if ( email.trim().length < 6 || fname.trim().length < 3 || lname.trim().length < 3 || address.trim().length < 5 || city.trim().length < 3 || state.trim().length < 3 || pincode.trim().length < 4 ) {
+      return res.render("payment.ejs", {
+        amount
+      });
+    } else {
+      return res.redirect("/carddetails");
+    }
 })
 
-router.get("/paytm", async (req, res) => {
-  return res.render("testpayment.ejs");
-})
+
 
 module.exports = router;
